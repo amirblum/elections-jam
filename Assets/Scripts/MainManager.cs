48b061d9d52d1b63ae996b495c5aa15c49be9b67
Amir Blum <amirblum@gmail.com>
2020-03-02 23:14:24 +0200

    Added end routine

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
@@ -9,6 +10,13 @@ public class MainManager : MonoBehaviour
     [SerializeField] Color _timerAlertColor;
     [SerializeField] float _startingTime;
     private float _timer;
+
+    [SerializeField] GameObject _winScreen;
+    [SerializeField] GameObject _loseScreen;
+    [SerializeField] GameObject _retryPrompt;
+    [SerializeField] float _retrySeconds;
+    private bool _canRetry;
+    [SerializeField] GameObject _loadingScreen;
 
     public static MainManager Instance;
 
@@ -32,13 +40,38 @@ public class MainManager : MonoBehaviour
         LoseGame();
     }
 
+    protected void Update()
+    {
+        if (_canRetry && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadingCoroutine());
+        }
+    }
+
     public void WinGame()
     {
         Debug.Log("You Win!");
+        StartCoroutine(EndCoroutine(_winScreen));
     }
 
     public void LoseGame()
     {
         Debug.Log("You Lose!");
+        StartCoroutine(EndCoroutine(_loseScreen));
+    }
+
+    private IEnumerator EndCoroutine(GameObject screen)
+    {
+        screen.SetActive(true);
+        yield return new WaitForSeconds(_retrySeconds);
+        _retryPrompt.SetActive(true);
+        _canRetry = true;
+    }
+
+    private IEnumerator LoadingCoroutine()
+    {
+        _loadingScreen.SetActive(true);
+        yield return new WaitForSeconds(_retrySeconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }

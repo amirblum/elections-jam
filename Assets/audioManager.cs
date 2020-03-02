7399f11f1803b3dd6e73a1b5ceb91a172fd29589
ben <benjamin.berthelot@gmail.com>
2020-03-02 23:27:17 +0200

    audio manager + start on 28 sec

---
+++ Assets/audioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioSource Src;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Src.time = 28f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

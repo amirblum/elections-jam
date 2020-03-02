9599ac84c2be697eef73e6ad86ea5008ffed0b25
Amir Blum <amirblum@gmail.com>
2020-03-02 19:55:39 +0200

    Added Player prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _walkSpeed;
+
+    private CharacterController _controller;
+
+    protected void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    
+    protected void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _controller.SimpleMove(Vector3.left * _walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _controller.SimpleMove(Vector3.right * _walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            _controller.SimpleMove(Vector3.forward * _walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _controller.SimpleMove(Vector3.back * _walkSpeed);
+        }
+    }
+}

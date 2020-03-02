1213c494b601590b2e47c031fa627be5339849a8
Amir Blum <amirblum@gmail.com>
2020-03-02 20:26:27 +0200

    Idle/run based on character speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,20 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController _controller;
     private Transform _body;
+    private Animator _animator;
 
     protected void Awake()
     {
         _controller = GetComponent<CharacterController>();
         _body = transform.GetChild(0);
+        _animator = GetComponentInChildren<Animator>();
     }
-    
+
     protected void Update()
     {
         var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _body.LookAt(transform.position + moveDirection);
+        _animator.SetFloat("Speed_f", moveDirection.magnitude);
 
         moveDirection *= _walkSpeed;
         _controller.Move(moveDirection * Time.deltaTime);
